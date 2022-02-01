40d1b4e4c4f4980c7fbc54c74269892e912a7cf0
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-02-01 12:37:42 +0600

    add localstorage

--- src/bll/store/store.ts
+++ src/bll/store/store.ts
@@ -1,5 +1,6 @@
 import {combineReducers, createStore} from "redux";
 import counterReducer from '../../bll/reducer/counterReducer'
+import {loadState, saveState } from "../utilities/utility";
 
 const rootReducer = combineReducers({
     counter: counterReducer
@@ -7,8 +8,16 @@ const rootReducer = combineReducers({
 
 export type AppRootState = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+export const store = createStore(rootReducer, loadState())
 
+
+
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter
+
+    });
+});
 
 // @ts-ignore
 window.store = store
